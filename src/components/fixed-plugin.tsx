ce09afe4033cb1ecdffb120ca89ef110e86b9350
Xmausix <barbara.szymanska469@gmail.com>
2024-11-15 18:00:49 +0100

    initial

---
+++ src/components/fixed-plugin.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Image from "next/image";
+import { Button } from "@material-tailwind/react";
+
+export function FixedPlugin() {
+    return (
+        <a href="https://github.com/Xmausix" target="_blank">
+            <Button
+                color="white"
+                size="sm"
+                className="!fixed bottom-4 right-4 flex gap-1 pl-2 items-center border border-blue-gray-50"
+            >
+
+                My Github
+            </Button>
+        </a>
+    );
+}
